306fe170aa514b220dcb2e2e2f636afad1427865
wnicolas <nicolas.buitrago2gmail.com>
2021-08-21 12:39:11 -0500

    Representante y usuario creados

--- src/main/java/DAO/RepresentanteVentasDAO.java
+++ src/main/java/DAO/RepresentanteVentasDAO.java
@@ -32,16 +32,22 @@ public class RepresentanteVentasDAO {
         PreparedStatement pstUsuario = null;
         PreparedStatement pstRepresentante = null;
 
-        String sqlUser = "create user " + usuario.getK_USUARIO() + " identified  by \"" + usuario.getK_USUARIO() + "\" default tablespace natdef temporary tablespace nattmp quota 2m on natdef password expire;";
-        String sqlUsuario = "INSERT INTO NATAME.USUARIO VALUES('" + usuario.getK_USUARIO() + "','" + usuario.getN_NOMBRE1() + "','" + usuario.getN_NOMBRE2() + "','" + usuario.getN_APELLIDO1() + "','" + usuario.getN_APELLIDO2() + "','" + usuario.getI_TIPO_DOCUMENTO() + "','" + usuario.getN_DOCUMENTO() + "','" + usuario.getN_DIRECCION() + "','" + usuario.getN_CORREO() + "','" + usuario.getI_GENERO() + "', TO_DATE('" + usuario.getF_NACIMIENTO() + "','YYYY-MM-DD'),'" + usuario.getN_TELEFONO() + "');";
-        String sqlRepresentante = "INSERT INTO NATAME.REPRESENTANTE_VENTAS VALUES('" + representante.getK_REPRESENTANTE() + "'," + representante.getK_REGION() + "," + representante.getK_PAIS() + ",'" + representante.getK_REPRESENTANTE_SUPERIOR() + "');";
+        String sqlUser = "create user " + usuario.getK_USUARIO() + " identified  by \"" + usuario.getK_USUARIO() + "\" default tablespace natdef temporary tablespace nattmp quota 2m on natdef password expire";
+        String sqlUsuario = "INSERT INTO NATAME.USUARIO VALUES('" + usuario.getK_USUARIO() + "','" + usuario.getN_NOMBRE1() + "','" + usuario.getN_NOMBRE2() + "','" + usuario.getN_APELLIDO1() + "','" + usuario.getN_APELLIDO2() + "','" + usuario.getI_TIPO_DOCUMENTO() + "','" + usuario.getN_DOCUMENTO() + "','" + usuario.getN_DIRECCION() + "','" + usuario.getN_CORREO() + "','" + usuario.getI_GENERO() + "', TO_DATE('" + usuario.getF_NACIMIENTO() + "','YYYY-MM-DD'),'" + usuario.getN_TELEFONO() + "')";
+        String sqlRepresentante = "INSERT INTO NATAME.REPRESENTANTE_VENTAS VALUES('" + representante.getK_REPRESENTANTE() + "'," + representante.getK_REGION() + "," + representante.getK_PAIS() + ",'" + representante.getK_REPRESENTANTE_SUPERIOR() + "')";
 
+        String sqlPrueba="SELECT * FROM USUARIO;";
+        
         System.out.println(sqlUser);
         System.out.println(sqlUsuario);
         System.out.println(sqlRepresentante);
 
         //System.out.println(sql);
         try {
+            
+//            pstUser = conn.prepareStatement(sqlPrueba);
+//            pstUser.execute();
+//            pstUser.close();
             
 //            pstUser = conn.prepareStatement(sqlUser);
 //            pstUser.execute();
