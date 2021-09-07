eca6ff228ea1f40a4b5f4f9b585520348646d03c
Leykat <lecvargasm@correo.udistrital.edu.co>
2021-09-07 17:47:25 -0500

    método agregarCliente en ClienteDAO

--- src/main/java/DAO/ClienteDAO.java
+++ src/main/java/DAO/ClienteDAO.java
@@ -21,18 +21,66 @@ public class ClienteDAO {
     public ClienteDAO() {
     }
 
-    public String agregarCliente(Connection conn,Cliente cliente) {
-        PreparedStatement pst = null;
-        String sql = "INSERT INTO natame.CLIENTE VALUES("+cliente.getK_CLIENTE()+")";
-        System.out.println(sql);
+//    public String agregarCliente(Connection conn,Cliente cliente) {
+//        PreparedStatement pst = null;
+//        String sql = "INSERT INTO natame.CLIENTE VALUES("+cliente.getK_CLIENTE()+")";
+//        System.out.println(sql);
+//
+//        try {
+//            pst = conn.prepareStatement(sql);
+//            pst.execute();
+//            pst.close();
+//            return "Guardado correctamente.";
+//
+//        } catch (SQLException e) {
+//            return e.getMessage();
+//        }
+//    }
+public String agregarCliente(Connection conn, Cliente cliente) throws SQLException {
 
+        PreparedStatement pstUser = null;
+        PreparedStatement pstRole = null;
+        PreparedStatement pstUsuario = null;
+        PreparedStatement pstRepresentante = null;
+        
+        String sqlUser = "CREATE USER " + cliente.getK_CLIENTE() + " IDENTIFIED  BY \"" + cliente.getK_CLIENTE() + "\" default tablespace natdef temporary tablespace nattmp quota 2m on natdef";
+        String sqlRole = "GRANT R_CLIENTE TO " + cliente.getK_CLIENTE();
+        //String sqlUsuario = "INSERT INTO NATAME.USUARIO VALUES('" + representante.getK_REPRESENTANTE() + "','" + representante.getN_NOMBRE1() + "','" + representante.getN_NOMBRE2() + "','" + representante.getN_APELLIDO1() + "','" + representante.getN_APELLIDO2() + "','" + representante.getI_TIPO_DOCUMENTO() + "','" + representante.getN_DOCUMENTO() + "','" + representante.getN_DIRECCION() + "','" + representante.getN_CORREO() + "','" + representante.getI_GENERO() + "', TO_DATE('" + representante.getF_NACIMIENTO() + "','YYYY-MM-DD'),'" + representante.getN_TELEFONO() + "')";
+        String sqlCliente = "INSERT INTO NATAME.REPRESENTANTE_VENTAS VALUES('" + cliente.getK_CLIENTE() + "','" +cliente.getN_NOMBRE1() + "','" + cliente.getN_NOMBRE2() + "','" + cliente.getN_APELLIDO1() + "','" + cliente.getN_APELLIDO2() + "','" + cliente.getI_TIPO_DOCUMENTO() + "','" + "','" + cliente.getN_DOCUMENTO() + "','" + cliente.getN_DIRECCION() + "','" + cliente.getN_CORREO() + "','" + cliente.getI_GENERO()+ "', TO_DATE('" + cliente.getF_NACIMIENTO() + "','YYYY-MM-DD'),'"  + "')";
+        //String sqlPrueba="SELECT * FROM USUARIO;";
+
+        System.out.println(sqlUser);
+        //System.out.println(sqlUsuario);
+        System.out.println(sqlCliente);
+
+        //System.out.println(sql);
         try {
-            pst = conn.prepareStatement(sql);
-            pst.execute();
-            pst.close();
+            pstUser = conn.prepareStatement(sqlUser);
+            pstUser.execute();
+            pstUser.close();
+
+            pstRole = conn.prepareStatement(sqlRole);
+            pstRole.execute();
+            pstRole.close();
+
+//            pstUsuario = conn.prepareStatement(sqlUsuario);
+//            pstUsuario.execute();
+//            pstUsuario.close();
+
+            pstRepresentante = conn.prepareStatement(sqlCliente);
+            pstRepresentante.execute();
+            pstRepresentante.close();
             return "Guardado correctamente.";
 
         } catch (SQLException e) {
+
+            PreparedStatement pstRollback = null;
+            String sqlRollback = "ROLLBACK"; 
+            pstRollback = conn.prepareStatement(sqlRollback);
+            pstRollback.execute();
+            pstRollback.close();
+
+            System.out.println(e);
             return e.getMessage();
         }
     }
