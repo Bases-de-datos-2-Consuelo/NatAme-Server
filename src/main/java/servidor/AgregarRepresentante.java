5c389a83c483bd7ff0256da0ddd02ede970b75a9
wnicolas <nicolas.buitrago2gmail.com>
2021-10-08 14:45:36 -0500

    Ya se puede crear un representante de ventas desde la interfaz

--- src/main/java/servidor/AgregarRepresentante.java
+++ src/main/java/servidor/AgregarRepresentante.java
@@ -9,8 +9,11 @@ import DAO.RepresentanteVentasDAO;
 import conexion.Conexion;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -36,7 +39,7 @@ public class AgregarRepresentante extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException {
         response.setContentType("application/json;charset=UTF-8");
         response.setHeader("Access-Control-Allow-Origin", "*");
 
@@ -54,6 +57,9 @@ public class AgregarRepresentante extends HttpServlet {
         String N_CORREO = request.getParameter("N_CORREO");
         String I_GENERO = request.getParameter("I_GENERO");
         String F_NACIMIENTO = request.getParameter("F_NACIMIENTO");
+        String fecha[] = F_NACIMIENTO.split("-");
+        F_NACIMIENTO = fecha[2] + '-' + fecha[1] + '-' + fecha[0];
+        
         String N_TELEFONO = request.getParameter("N_TELEFONO");
         System.out.println(K_REPRESENTANTE);
         System.out.println(K_REGION);
@@ -72,31 +78,57 @@ public class AgregarRepresentante extends HttpServlet {
         System.out.println(N_TELEFONO);
 
         // response.sendRedirect("https://www.it-swarm-es.com/es/java/llamar-una-funcion-de-oracle-desde-java/1068792680/");
-//        String user = request.getParameter("user");
-//        String password = request.getParameter("password");
-//        Conexion c = new Conexion("natame", "natame1234"); //Provicionalmente pongo valores quemados, pero ya trae el user y password de un formualrio
-        /*Connection co = Conexion.getConnection();
+        //String user = request.getParameter("user");
+        //String password = request.getParameter("password");
+        Conexion c = new Conexion("natame", "natame1234"); //Provicionalmente pongo valores quemados, pero ya trae el user y password de un formualrio
+        Connection connection = Conexion.getConnection();
+        CallableStatement cs = null;
 
-        RepresentanteVentasDAO representanteDAO = new RepresentanteVentasDAO();
-        Representante_Ventas representante = new Representante_Ventas(request.getParameter("K_REPRESENTANTE"), request.getParameter("K_REPRESENTANTE_SUPERIOR"), Integer.parseInt(request.getParameter("K_REGION")), Integer.parseInt(request.getParameter("K_PAIS")), request.getParameter("N_NOMBRE1"), request.getParameter("N_NOMBRE2"), request.getParameter("N_APELLIDO1"), request.getParameter("N_APELLIDO2"), request.getParameter("I_TIPO_DOCUMENTO"), request.getParameter("N_DOCUMENTO"), request.getParameter("N_DIRECCION"), request.getParameter("N_CORREO"), request.getParameter("I_GENERO"), request.getParameter("F_NACIMIENTO"), request.getParameter("N_TELEFONO"));
-        //Usuario usuario = new Usuario(request.getParameter("K_USUARIO"), request.getParameter("N_NOMBRE1"), request.getParameter("N_NOMBRE2"), request.getParameter("N_APELLIDO1"), request.getParameter("N_APELLIDO2"), request.getParameter("I_TIPO_DOCUMENTO"), request.getParameter("N_DOCUMENTO"), request.getParameter("N_DIRECCION"), request.getParameter("N_CORREO"), request.getParameter("I_GENERO"), request.getParameter("F_NACIMIENTO"), request.getParameter("N_TELEFONO"));
+        //Se realiza la llamada a la funcion de BBDD que retornará un String
+        cs = connection.prepareCall("{? = call FU_AGREGAR_REPVENTA(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}");
 
-        String respuesta;
-        try {
-            respuesta = representanteDAO.agregarRepresentante(co, representante);
-        } catch (SQLException ex) {
-            Logger.getLogger(AgregarRepresentante.class.getName()).log(Level.SEVERE, null, ex);
+        cs.registerOutParameter(1, Types.VARCHAR); //se indica el objeto de salida y la posición, en este caso un String.
+        cs.setString(2, K_REPRESENTANTE);
+        cs.setString(3, K_REGION);
+        cs.setString(4, K_PAIS);
+        cs.setString(5, K_REPRESENTANTE_SUPERIOR);
+        cs.setString(6, N_NOMBRE1);
+        cs.setString(7, N_NOMBRE2);
+        cs.setString(8, N_APELLIDO1);
+        cs.setString(9, N_APELLIDO2);
+        cs.setString(10, I_TIPO_DOCUMENTO);
+        cs.setString(11, Q_DOCUMENTO);
+        cs.setString(12, N_DIRECCION);
+        cs.setString(13, N_CORREO);
+        cs.setString(14, I_GENERO);
+        cs.setString(15, F_NACIMIENTO);
+        cs.setString(16, N_TELEFONO);
+
+        cs.execute(); //ejecuta la llamada y retorna un boolean, se puede usar también executeUpdate() que retorna un entero.
+
+        //se recupera el resultado de la funcion pl/sql
+        String retorno = cs.getString(1);
+
+        if (retorno.equalsIgnoreCase("ok")) {
+            PreparedStatement pstUser = null;
+            String sqlUser = "CREATE USER " + K_REPRESENTANTE + " IDENTIFIED BY " + K_REPRESENTANTE + " DEFAULT TABLESPACE usernatdef temporary tablespace usernattmp quota 2m on usernatdef";
+            System.out.println(sqlUser);
+            pstUser = connection.prepareStatement(sqlUser);
+            pstUser.execute();
+            pstUser.close();
+
+            PreparedStatement pstGrant = null;
+            String sqlGrant = "GRANT R_REPRESENTANTEVENTA TO " + K_REPRESENTANTE;
+            pstGrant = connection.prepareStatement(sqlGrant);
+            pstGrant.execute();
+            pstGrant.close();
         }
-        //String respuesta="hOLI";
-        /*
-        //
-       String 
-        respuesta="hOLA";*/
+
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("[");
             out.println("{");
-            out.println("\"respuesta\": \"" + "Hola mundo desde Agregar representante de ventas" + '"');
+            out.println("\"respuesta\": \"" + retorno + '"');
             out.println("}");
             out.println("]");
 
@@ -115,7 +147,11 @@ public class AgregarRepresentante extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(AgregarRepresentante.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -129,7 +165,11 @@ public class AgregarRepresentante extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(AgregarRepresentante.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
